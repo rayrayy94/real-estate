b899b0190cd5bf61db330155c95aeeb47ebf4fd7
Rehan Arshad <rehanarshad@Rehans-MacBook-Air.local>
2024-06-17 22:07:38 -0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 // import express from "express"; -- ADD "type": "module" in package.json under main
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 
