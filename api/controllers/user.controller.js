a0430372c3d11426c95444ecbece910d42537515
Rehan Arshad <rehanarshad@Rehans-MacBook-Air.local>
2024-06-26 14:45:26 -0500

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,7 @@
+const test = (req, res) => {
+  res.json({
+    message: "API route is working",
+  });
+};
+
+module.exports = { test };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose"); //for database
 const dotenv = require("dotenv"); //import dotenv after npm i dotenv
 dotenv.config(); //initialize the env file to be used when importing url link from .env file
+const userRouter = require("./routes/user.route.js");
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,6 +15,8 @@ mongoose
   });
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Port is running on 3000");
